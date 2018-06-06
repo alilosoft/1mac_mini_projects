4fd6cca22cb6143d85acfac1b3fd44256c4900ec
@lilo$oft <fellahi.ali@gmail.com>
2018-06-06 19:27:29 +0100

    Edit print message

--- 01_Take_Break/take_break.py
+++ 01_Take_Break/take_break.py
@@ -4,9 +4,9 @@ import time
 times = 5
 print "Start working on: " + time.ctime()
 while times > 0:
-    webbrowser.open("https://classroom.udacity.com/courses/ud004-track-1mac")
+    print "It's time for work!"
     time.sleep(25 * 60)
-    print "Time for break!"
+    print "Now, It's time to take a break!"
     webbrowser.open("https://www.youtube.com/watch?v=1g5wM-YSNHo")
     time.sleep(5 * 60)
     times -= 1
