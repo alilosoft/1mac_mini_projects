a46d93a4192ae770998bd449b5310afc35c57a23
@lilo$oft <fellahi.ali@gmail.com>
2018-06-12 06:21:51 +0100

    Adds check_profanity Mini-Project

--- 05_Profanity_Filter/check_profanity.py
+++ 05_Profanity_Filter/check_profanity.py
@@ -1,8 +1,18 @@
+import urllib
 
-def read_txt():
-    txt_file = open("check_profanity.py")
+
+def read_txt(txt_file_path):
+    txt_file = open(txt_file_path)
     file_content = txt_file.read()
-    print file_content
     txt_file.close()
+    return file_content
 
-READ_TXT()
+
+def check_profanity(text):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" + text)
+    response = connection.read()
+    connection.close()
+    return response
+
+
+print check_profanity(read_txt("story.txt"))
