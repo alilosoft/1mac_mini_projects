f0c8d96e9d28e7c271b29db50e4b072b0fbcd638
@lilo$oft <fellahi.ali@gmail.com>
2018-06-26 23:44:19 +0100

    Adds Movies_Website Mini-Project

--- 06_Movies_Website/ent_center.py
+++ 06_Movies_Website/ent_center.py
@@ -1,14 +1,46 @@
 # encoding: utf-8
 import media
+import fresh_tomatoes
 
-title = "حتة حكمة - حاول مرة كمان"
-story_line = """معظمنا لما بنبدأ مشروع جديد أو بنحاول نكتسب  مهارة أو عادة جديدة في حياتنا  
-بيكون عندنا أمل - أو وهم - إن كل شيء هيمشي حسب الخطة الموضوعة
- وبأننا هننجح بدون معوقات ومن المحاولة الأولى!"""
-img_url = "img_url"
-youtube_url = "https://www.youtube.com/watch?v=lUB89q_FIug"
+title = "علي وكتاب - الفارق البسيط The Slight Edge"
+story_line = """أول حلقات برنامج علي وكتاب مع كتاب 
+The Slight Edge .. أو الفارق البسيط
+مع هذا الكتاب نتعلم لماذا يفشل الناس في تطبيق ما يعرفونه من معرفة مفيدة .. وما الحل الأمثل للتغيير وتحقيق النجاح."""
+img_url = "http://img.youtube.com/vi/biCRsdst958/0.jpg"
+youtube_url = "https://www.youtube.com/watch?v=biCRsdst958&list=PLDN8vU_ZehsW7bFxs0yG5yNImjueqo3UY&index=2"
+slight_edge = media.Movie(title, story_line, img_url, youtube_url)
 
-hikma = media.Movie(title, story_line, img_url, youtube_url)
+# print slight_edge.story_line
+# slight_edge.show_trailer()
 
-print hikma.story_line
-hikma.show_trailer()
+title = "علي وكتاب - حل لغز التسويف Solving The Procrastination Puzzle"
+story_line = """كتاب حل لغز التسويف المماطلة .. هنتعرف من خلاله على أسباب التسويف ومخاطره وكيفية علاجه."""
+img_url = "http://img.youtube.com/vi/22CW76-SARA/0.jpg"
+youtube_url = "https://www.youtube.com/watch?v=22CW76-SARA&index=3&list=PLDN8vU_ZehsW7bFxs0yG5yNImjueqo3UY"
+procrastination_puzzle = media.Movie(title, story_line, img_url, youtube_url)
+
+
+title = "علي وكتاب - قوة الإرادة Will Power"
+story_line = """نتعرف في حلقة اليوم على ما هي قوة الإرادة وما طبيعتها وكيف تقوي عضلة قوة الإرادة عندك
+سنتعرف على تأثير المشاعر والبيئة المحيطة على قوة الإرادة 
+سنتكلم عن بناء العادات وحرق السفن والتنفس بعمق
+ستتعلم كيف تمتلك قوة الإرادة لتحقيق النجاح"""
+img_url = "http://img.youtube.com/vi/THnrunRXYss/0.jpg"
+youtube_url = "https://www.youtube.com/watch?v=THnrunRXYss&index=6&list=PLDN8vU_ZehsW7bFxs0yG5yNImjueqo3UY"
+will_power = media.Movie(title, story_line, img_url, youtube_url)
+
+title = "علي وكتاب - العمل العميق Deep Work"
+story_line = """مع كتاب اليوم سنتعلم أهمية وكيفية العمل بتركيز لتحقيق إنتاجية أكبر في وقت أقل"""
+img_url = "http://img.youtube.com/vi/o1wHtClg6OQ/0.jpg"
+youtube_url = "https://www.youtube.com/watch?v=o1wHtClg6OQ&index=9&list=PLDN8vU_ZehsW7bFxs0yG5yNImjueqo3UY"
+deep_work = media.Movie(title, story_line, img_url, youtube_url)
+
+title = "خمس كتب في خمس دقائق! - علي وكتاب"
+story_line = """في هذا الفيديو نعرض خمس كتب في خمس دقائق .. لكن لماذا يا ترى؟"""
+img_url = "http://img.youtube.com/vi/FQRMYuIzNjA/0.jpg"
+youtube_url = "https://www.youtube.com/watch?v=FQRMYuIzNjA&index=16&list=PLDN8vU_ZehsW7bFxs0yG5yNImjueqo3UY"
+five_books = media.Movie(title, story_line, img_url, youtube_url)
+
+
+movies = [slight_edge, procrastination_puzzle, will_power, deep_work, five_books]
+fresh_tomatoes.open_movies_page(movies)
--- 06_Movies_Website/media.py
+++ 06_Movies_Website/media.py
@@ -5,8 +5,8 @@ class Movie:
         print "init Movie: " + str(self)
         self.title = title
         self.story_line = story_line
-        self.poster_img_url = poster_img_url
-        self.trailer_url = trailer_url
+        self.poster_image_url = poster_img_url
+        self.trailer_youtube_url = trailer_url
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_url)
+        webbrowser.open(self.trailer_youtube_url)
